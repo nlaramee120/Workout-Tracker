78d9de97e757b15201be1beaa7e6503d2e795627
nlaramee120 <nlaramee120@aol.com>
2021-12-04 08:27:13 -0600

    Created 3 routes for the homepage.

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,74 @@
 const router = require("express").Router();
-const db = require("../models/workout.js");
+const db = require("../models");
 
+router.post("/api/workouts", ({ body }, res) => {
+  db.Workout.newWorkout(body)
 
+    .then((data) => {
+      res.json(data);
+    })
+
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  db.Workout.updateOne(
+
+    { _id: req.params.id },
+
+    {
+      $inc: { totalDuration: req.body.duration },
+      $push: { exercises: req.body },
+    },
+
+    { new: true }
+
+  )
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then((data) => {
+      data.forEach((workout) => {
+        var total = 0;
+        workout.exercises.forEach((e) => {
+          total += e.duration;
+        });
+        workout.totalDuration = total;
+      });
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.range([{
+        $project:
+        {
+            day: "$day",
+            totalDuration: { $sum: "$exercises.duration" },
+            totalDistance: { $sum: "$exercises.distance" },
+            totalweight: { $sum: "exercises.weight" },
+            exercises: "$exercises"
+        }
+  }])
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 
 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan")
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3007;
 
 const app = express();
 
