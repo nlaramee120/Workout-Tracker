e3738d7852c403a00556878d0af989ae9573a60e
nlaramee120 <nlaramee120@aol.com>
2021-12-03 22:45:14 -0600

    Created the model for the workouts and started working on the routes.

---
+++ routes/api.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const db = require("../models/workout.js");
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan")
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,6 +15,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
