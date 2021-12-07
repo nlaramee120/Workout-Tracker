648e24ee6b615283acfe887bce1a119cf8f4e697
nlaramee120 <nlaramee120@aol.com>
2021-12-06 19:03:35 -0600

    Worked on the last 3 routes for the api.

--- routes/api.js
+++ routes/api.js
@@ -13,6 +13,63 @@ router.post("/api/workouts", ({ body }, res) => {
     });
 });
 
+router.put("/api/workouts/:id", (req, res) => {
+  db.Workout.findOneAndUpdate(
+
+    { _id: req.params.id },
+
+    {
+      $inc: { totalDuration: req.body.duration },
+      $push: { exercises: req.body },
+    },
+
+    { new: true }
+
+  )
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then((data) => {
+      data.forEach((workout) => {
+        var total = 0;
+        workout.exercises.forEach((e) => {
+          total += e.duration;
+        });
+        workout.totalDuration = total;
+      });
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.aggregate([{
+        $project:
+        {
+            day: "$day",
+            totalDuration: { $sum: "$exercises.duration" },
+            totalDistance: { $sum: "$exercises.distance" },
+            totalweight: { $sum: "exercises.weight" },
+            exercises: "$exercises"
+        }
+  }])
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
 
 
 
